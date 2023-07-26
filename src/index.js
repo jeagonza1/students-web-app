a467352fc449bc83e78c9ebebfc44602113423e0
Jean <jfga1508@gmail.com>
2023-07-26 11:43:59 -0500

    changed the correct client uri

--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { createRoot } from 'react-dom/client';
 
 // Usage:
 
-
 const client = new ApolloClient({
-  //uri: "http://localhost:8080/student-service",
+    //uri: "http://localhost:8080/student-service",
 
-  uri: "https://webgraphql-api-student.azurewebsites.net/student-service",
-  cache: new InMemoryCache()
+    uri: 'https://studentsapiweb.azurewebsites.net/student-service',
+    cache: new InMemoryCache(),
 });
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <ApolloProvider client={client}>
+                <App />
+            </ApolloProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
